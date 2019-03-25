ae21c5a378c4f281dc33050ad3ab7fe075cbd6d3
马浩哲 <411408809@qq.com>
2019-03-25 11:22:52 +0800

    添加pod

--- MHZAssembly/AppDelegate.swift
+++ MHZAssembly/AppDelegate.swift
@@ -15,7 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        self.window = UIWindow.init(frame: UIScreen.main.bounds)
+        self.window?.backgroundColor = UIColor.white
+        
+        let rootVC = ViewController()
+        self.window!.rootViewController = rootVC
+        self.window!.makeKeyAndVisible()
+        
         return true
     }
 
